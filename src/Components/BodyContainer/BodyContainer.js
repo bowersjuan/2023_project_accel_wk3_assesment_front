039eb9be0f65610d05eb8e12d76b98313fea1a61
bowersjuan <bowersjuan@gmail.com>
2023-08-02 05:34:34 -0400

    Update BodyContainer with conditional classNames for centering Loading and Error components

--- src/Components/BodyContainer/BodyContainer.js
+++ src/Components/BodyContainer/BodyContainer.js
@@ -1,14 +1,13 @@
 import "./BodyContainer.css";
 
-export default function BodyContainer({ children, grid, overflow }) {
+export default function BodyContainer({ children, grid, center }) {
   let classNames = ["BodyContainer"];
 
   if (grid) {
     classNames.push("BodyContainer--grid");
-  }
-
-  if (overflow) {
     classNames.push("BodyContainer--overflow");
+  } else if (center) {
+    classNames.push("BodyContainer--center");
   }
 
   return <div className={classNames.join(" ")}>{children}</div>;
