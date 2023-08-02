e72658fe4bd182079f99f31ab80bd419772a7f76
bowersjuan <bowersjuan@gmail.com>
2023-08-02 00:29:46 -0400

    Update App.js with helper functions and Loading, Error and ItemList components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,44 @@
+// Imports
+import { useEffect, useState } from "react";
 import "./App.css";
 
+// Components
+import Error from "./Components/Error/Error";
+import Loading from "./Components/Loading/Loading";
+import ItemList from "./Components/ItemList/ItemList";
+
+// Helpers
+import renderContent from "./Helpers/renderContent";
+import fetchData from "./Helpers/fetchData";
+
+const API = process.env.REACT_APP_API_URL;
+const Route = "items";
+
 function App() {
-  return <div className="App">Food App</div>;
+  const [items, setItems] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
+
+  const renderedContent = renderContent(
+    loading,
+    <Loading />,
+    error,
+    <Error />,
+    <ItemList data={items} />
+  );
+
+  useEffect(() => {
+    console.log("useEffect fires");
+    fetchData(API, Route, setLoading, setError, setItems);
+  }, []);
+
+  console.log("App renders");
+  return (
+    <div className="App">
+      <h1>Food App</h1>
+      <div>{renderedContent}</div>
+    </div>
+  );
 }
 
 export default App;
