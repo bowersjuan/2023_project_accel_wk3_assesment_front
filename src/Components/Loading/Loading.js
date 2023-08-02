95b9af1a6959666847d04d6c238fdd5f09801d1b
bowersjuan <bowersjuan@gmail.com>
2023-08-02 05:35:39 -0400

    Update and style Loading component

--- src/Components/Loading/Loading.js
+++ src/Components/Loading/Loading.js
@@ -1,3 +1,11 @@
+import { Puff } from "react-loading-icons";
+import "./Loading.css";
+
 export default function Loading() {
-  return <div className="Loading...">Loading</div>;
+  return (
+    <div className="Loading">
+      <strong>Loading</strong>
+      <Puff stroke="#fff" strokeOpacity={0.125} speed={0.7} />
+    </div>
+  );
 }
