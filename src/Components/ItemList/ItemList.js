b8177b9755d947cc6efb73076a43a4d0d50b89e1
bowersjuan <bowersjuan@gmail.com>
2023-08-04 09:56:27 -0400

    Update ItemList and style BodyContainer for better reusability with grid

--- src/Components/ItemList/ItemList.js
+++ src/Components/ItemList/ItemList.js
@@ -2,10 +2,10 @@ import ItemCard from "../ItemCard/ItemCard";
 
 export default function ItemList({ data }) {
   return (
-    <div className="ItemList">
+    <>
       {data.map((item) => {
         return <ItemCard key={item.id} item={item} />;
       })}
-    </div>
+    </>
   );
 }
